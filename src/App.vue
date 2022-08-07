af0a414412ed35069994fdb6c24beddfd614f68b
Lluc Pares <lluc.pares@outlook.com>
2022-08-07 11:17:53 +0200

    Init markdown-component
    This component will be able to render an HTML by parsing the
    following custom markdown tags:
    - **something**: renders a <b>something</b>
    - ~~something~~: renders a <s>something</s>
    - <color>s<color>: renders a text with a <span class="color"> tag
    - <caution>s<caution>: renders a text with a <div clas="caution">
    tag
    - \n: renders a <br> tag
    - <example>s<example>: renders a text with a <div class="example">
    tag
    - <tip>s<tip>: renders a text with a <div class="tip"> tag
    - {ab}(cd): renders a text ab with cd on top <span class="kanji">
    ab<span>cd></span></span>

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<textarea v-model="input"></textarea>
+<br>
+<br>
+<MarkdownCompiler :message="input" :key="input"/>
+<br>
+<br>
+<button v-on:click="copyToClipboard">Copy markdown to clipboard</button>
+</template>
+
+<script>
+import MarkdownCompiler from '@/components/markdownCompiler.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MarkdownCompiler
+  },
+  data(){
+    return {
+      input: "",
+    }
+  },
+  methods: {
+    copyToClipboard(){
+      navigator.clipboard.writeText(this.input);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/markdownCompiler.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container" v-html="sanitize(parse(message))"></div>
+</template>
+<script>
+const sanitizeHtml = require('sanitize-html');
+
+const sanitizeOptions = {
+  allowedTags: ['div', 'br', 'span', 'b', 's'],
+  allowedAttributes: {
+    'div': [ 'class' ],
+    'span': [ 'class' ]
+  }
+};
+
+export default{
+  name: 'markdownCompiler',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    parse(text){
+      if(!text) return "";
+      const toHtml = text
+            //Do not allow line jumps when there is a <caution>, <example> or <tip> tag
+            //since they render into div which will be already with some margins
+            .replace(/\n*<(\/?example|\/?caution|\?tip)>\n*/gm, "<$1>")
+            // Replace ** something ** with <b>something</b>
+            .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
+            //Replace <color>something</color> for <span class="color">something</span>
+            .replace(/<color>(.*?)<\/color>/gims, "<span class='color'>$1</span>")
+            //Replace multiple \r\n or \n for one <br>
+            .replace(/[\r\n]+/gm, "<br>")
+            //Replace <caution>something</caution> for <div class="caution">something</div>
+            .replace(/<caution>(.*?)<\/caution>/gims, "<div class='caution'>$1</div>")
+            //Replace <example>something</example> for <div class="example">something</div>
+            .replace(/<example>(.*?)<\/example>/gims, "<div class='example'>$1</div>")
+            //Replace <example>something</example> for <div class="example">something</div>
+            .replace(/<tip>(.*?)<\/tip>/gims, "<div class='tip'>$1</div>")
+            //Replace ~~something~~ for <s>something</s>
+            .replace(/~~(.*)~~/gim, "<s>$1</s>")
+            //Replace [ab]{cda} for <span class="kanji">ab<span>cd</span></span>
+            .replace(/\[([^\][]+)\]\{([^{}]+)\}/gim, "<span class='kanji'>$1<span>$2</span></span>")
+      return toHtml.trim();
+    },
+    sanitize(text){
+      return sanitizeHtml(text, sanitizeOptions);
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  padding: 10px;
+  border: 1px solid;
+  border-color: black;
+}
+
+.color {
+  color: green;
+}
+
+.caution {
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid;
+  border-color: red;
+}
+
+.caution > .example {
+  padding: 10px;
+  margin: 10px;
+  border: 1px dashed;
+  border-color: blue;
+}
+
+.example {
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid;
+  border-color: blue;
+}
+
+.tip {
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid;
+  border-color: green;
+}
+
+/* Display kanji with furigana on top */
+.kanji {
+  display: inline-flex;
+  flex-direction: column-reverse;
+  text-align: center;
+}
+
+/* Furigana */
+.kanji > span {
+  font-size: 10px;
+}
+</style>
